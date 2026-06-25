53d0e37f82c240c91c0caaf259ca27e57b1801a6
albertojrc <beto13ramirez32@gmail.com>
2026-06-25 17:48:23 +0200

    Apply light blue theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { App } from './app/App'
 import './styles/global.css'
 import './styles/mlCharts.css'
+import './styles/lightBlueTheme.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
