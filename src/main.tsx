79e5b5b47570810f86bb061f79d3d9e90bb24d1a
albertojrc <beto13ramirez32@gmail.com>
2026-06-27 12:57:14 +0200

    Import knowledge OS styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App'
 import './styles/global.css'
 import './styles/mlCharts.css'
 import './styles/lightBlueTheme.css'
+import './styles/knowledgeOS.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
