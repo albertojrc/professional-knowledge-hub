7027cabfd0c3c9d87e6861bafaa5ba71cf1b9c70
albertojrc <beto13ramirez32@gmail.com>
2026-06-27 21:22:28 +0200

    Register Sprint 2.4 source-aware assets

--- src/data/knowledgeAssetRegistry.ts
+++ src/data/knowledgeAssetRegistry.ts
@@ -1,5 +1,6 @@
 import { knowledgeAssets as coreKnowledgeAssets } from './knowledgeAssets'
 import { sprint16KnowledgeAssets } from './knowledgeAssetsSprint16'
 import { sprint14KnowledgeAssets } from './knowledgeAssetsSprint14'
+import { sprint24KnowledgeAssets } from './knowledgeAssetsSprint24'
 
-export const knowledgeAssetRegistry = [...coreKnowledgeAssets, ...sprint16KnowledgeAssets, ...sprint14KnowledgeAssets]
+export const knowledgeAssetRegistry = [...coreKnowledgeAssets, ...sprint16KnowledgeAssets, ...sprint14KnowledgeAssets, ...sprint24KnowledgeAssets]
