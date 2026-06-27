6fd1e40a55ec393841fa036c709d3d29b7614774
albertojrc <beto13ramirez32@gmail.com>
2026-06-27 19:46:32 +0200

    Import library filter styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './styles/lightBlueTheme.css'
 import './styles/knowledgeOS.css'
 import './styles/searchOS.css'
 import './styles/deepLinkOS.css'
+import './styles/libraryFilterOS.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
