d778d457a116799892778e9683ef5be2d14e5021
albertojrc <beto13ramirez32@gmail.com>
2026-06-27 20:25:57 +0200

    Register Sprint 1.14 tooltips

--- src/data/knowledgeTooltipRegistry.ts
+++ src/data/knowledgeTooltipRegistry.ts
@@ -1,9 +1,11 @@
 import { getKnowledgeTooltip, knowledgeTooltips, type KnowledgeTooltip } from './knowledgeTooltips'
 import { sprint16KnowledgeTooltips } from './knowledgeTooltipsSprint16'
+import { sprint14KnowledgeTooltips } from './knowledgeTooltipsSprint14'
 
 const tooltipRegistry: Record<string, KnowledgeTooltip> = {
   ...knowledgeTooltips,
-  ...sprint16KnowledgeTooltips
+  ...sprint16KnowledgeTooltips,
+  ...sprint14KnowledgeTooltips
 }
 
 export function getTooltipDefinition(label: string): KnowledgeTooltip | undefined {
