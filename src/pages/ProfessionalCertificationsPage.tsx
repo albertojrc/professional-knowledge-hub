a3b0b7fd85952ec4a3990e45e229004cc10685f7
albertojrc <beto13ramirez32@gmail.com>
2026-07-02 01:14:13 +0200

    Keep certification page route props compatible

--- src/pages/ProfessionalCertificationsPage.tsx
+++ src/pages/ProfessionalCertificationsPage.tsx
@@ -1,7 +1,8 @@
+import type { ViewId } from '../types/knowledge'
 import { cfaCurriculum } from '../data/moduleCurriculum'
 import { ModuleCurriculumStudio } from '../components/learning/ModuleCurriculumStudio'
 
-interface ProfessionalCertificationsPageProps { focusId?: string | null; onOpenAsset?: (assetId: string) => void }
+interface ProfessionalCertificationsPageProps { focusId?: string | null; onNavigate?: (view: ViewId, focusId?: string | null) => void; onOpenAsset?: (assetId: string) => void }
 
 export function ProfessionalCertificationsPage({ focusId, onOpenAsset }: ProfessionalCertificationsPageProps) {
   return <ModuleCurriculumStudio curriculum={cfaCurriculum} focusId={focusId} onOpenAsset={onOpenAsset ?? (() => {})} />
