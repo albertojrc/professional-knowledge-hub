a084786a2658df81ed42a8fd009ddb5f09e4e91e
albertojrc <beto13ramirez32@gmail.com>
2026-07-02 00:45:43 +0200

    Add governance visual completion registry

---
+++ src/data/visualGovernanceCompletion.ts
@@ -0,0 +1,5 @@
+export const visualGovernanceCompletion = {
+  status: 'complete',
+  activeWrappers: ['ABTGovernanceVisual', 'VisualGovernanceStudio', 'ControlledExperimentVisual', 'ModelHandoffVisual', 'PortfolioMonitoringVisual', 'ActionWorkflowVisual'],
+  note: 'Sprint 5.22 closes the last governance visual gap through ControlledExperimentVisual.'
+}
