f979584428eb73063f1d4da22adbf408bb32d795
albertojrc <beto13ramirez32@gmail.com>
2026-07-02 10:18:10 +0200

    Register CFA foundation knowledge assets

--- src/data/knowledgeAssetRegistry.ts
+++ src/data/knowledgeAssetRegistry.ts
@@ -2,5 +2,6 @@ import { knowledgeAssets as coreKnowledgeAssets } from './knowledgeAssets'
 import { sprint16KnowledgeAssets } from './knowledgeAssetsSprint16'
 import { sprint14KnowledgeAssets } from './knowledgeAssetsSprint14'
 import { sprint24KnowledgeAssets } from './knowledgeAssetsSprint24'
+import { cfaFoundationAssets } from './knowledgeAssetsCfa'
 
-export const knowledgeAssetRegistry = [...coreKnowledgeAssets, ...sprint16KnowledgeAssets, ...sprint14KnowledgeAssets, ...sprint24KnowledgeAssets]
+export const knowledgeAssetRegistry = [...coreKnowledgeAssets, ...sprint16KnowledgeAssets, ...sprint14KnowledgeAssets, ...sprint24KnowledgeAssets, ...cfaFoundationAssets]
